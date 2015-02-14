2d6a105c90b060b56f3c806418abdf2b63774fe4
gundz <fgundlac@student.42.fr>
2015-02-14 11:04:45 +0100

    solved compilation issue

--- srcs/ft_ctablen.c
+++ srcs/ft_ctablen.c
@@ -1,9 +1,11 @@
+#include <stddef.h>
+
 unsigned int				ft_ctablen(const char **const tab)
 {
 	unsigned int			len;
 
 	len = 0;
-	while (tab[i] != NULL)
-		tab[i]++;
+	while (tab[len] != NULL)
+		tab[len]++;
 	return (len);
 }
